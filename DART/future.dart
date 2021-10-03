ba7fa80d801a8a35230d364a1934d601f54c68ae
Ashish Kumar <70542481+ashishworkspace@users.noreply.github.com>
2021-10-03 09:16:28 +0530

    Future in Dart
    Use of await and async

---
+++ DART/future.dart
@@ -0,0 +1,13 @@
+// Future in Dart
+Future<void> main() async {
+  print(await showUserData());
+}
+
+Future<String> fetchUserData() async {
+  return await Future.delayed(Duration(seconds: 3), () => "Nice");
+}
+
+Future<String> showUserData() async {
+  String data = await fetchUserData();
+  return "This is user data: $data";
+}
